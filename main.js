0a57ad12b9028869d59d00985ed31f3f5b28d407
Ihor Lemeshko <le01n@ukr.net>
2022-09-16 13:34:12 +0300

    first task

--- main.js
+++ main.js
@@ -1,6 +1,42 @@
 "use strict";
-console.log('123');
-var as = 123;
-sr= 34
 
-console.log(sr);
+/* Задание на урок:
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
+Проверить, чтобы все работало без ошибок в консоли */
+
+const numberOfFilms = prompt('Сільки фільмів ви вже подивилися?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const lastWatchedFilmName = prompt('Один з останніх фільмів який Ви подивилися?', '');
+const lastWatchedFilmRating = prompt('На сколько оціните його?', '');
+
+personalMovieDB.movies[lastWatchedFilmName] = lastWatchedFilmRating;
+
+const lastWatchedFilmName2 = prompt('Один з останніх фільмів який Ви подивилися?', '');
+const lastWatchedFilmRating2 = prompt('На сколько оціните його?', '');
+
+personalMovieDB.movies[lastWatchedFilmName2] = lastWatchedFilmRating2;
+
+
+console.log(personalMovieDB);
